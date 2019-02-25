6598a50a0bb7519cfd1c09f20c09e14657a86e87
Nir Oren <nir.oren@here.com>
2019-02-25 15:08:47 +0200

    add clickVsDrag

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
-import { fromEvent, timer } from 'rxjs';
-import  { map, bufferTime, debounce, tap, buffer, filter } from 'rxjs/operators';
-
-//create observable that emits click events
-const source$ = fromEvent(document, 'click');
-//map to string with given event timestamp
-const example = source$.pipe(
-    map(event => `Event time: ${event.timeStamp}`),
-    buffer(
-        source$.pipe(debounce(() => timer(250))
-    )),
-    tap(arr => console.log('clicks', arr.length)),
-    filter(arr => arr.length > 1),
-    // tap(arr =>)
-
-);
+import { fromEvent, timer, merge } from 'rxjs';
+import  { map, debounce, tap, buffer, filter, mapTo, sample } from 'rxjs/operators';
 
 
-//output (example): 'Event time: 7276.390000000001'
-const subscribe = example.subscribe(val => console.log('double click occur', val));
+
+// doubleClick();
+clickVsDrag();
 
 
+
+
+
+/// Double click example 
+function doubleClick() {
+    
+    
+    //create observable that emits click events
+    const source$ = fromEvent(document, 'click');
+    const example = source$.pipe(map(event => `Event time: ${event.timeStamp}`), buffer(source$.pipe(debounce(() => timer(250)))), tap(arr => console.log('clicks', arr.length)), filter(arr => arr.length > 1));
+    //output (example): 'Event time: 7276.390000000001'
+    const subscribe = example.subscribe(val => console.log('double click occur', val));
+}
+
+function clickVsDrag() {
+    const listener = merge(
+        fromEvent(document, 'mousedown').pipe(mapTo(false)), 
+        fromEvent(document, 'mousemove').pipe(mapTo(true))
+    )
+        .pipe(
+            sample(fromEvent(document, 'mouseup')),
+        )
+        .subscribe(isDragging => {
+            console.log('Were you dragging?', isDragging);
+        });
+}
